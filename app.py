57f76e63aae42a8db4ff497b63ad38d2a41b2b07
iknow181 <1453803271@qq.com>
2024-04-10 14:27:27 +0800

    'update'

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
+import flask
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='/static',  # 静态文件路径
+            static_folder='static',
+            template_folder='templates'  # 模板文件
+            )
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def welcome():
+    return flask.render_template('welcome.html')
 
+# @app.route('/menu')
+# def menu():
+#
+#     return flask.render_template('')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8888, debug=True)
