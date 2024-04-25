f1a5b3af0c1ffec050d264527f065642b52ac0e6
iknow181 <134586211+iknow181@users.noreply.github.com>
2024-04-26 01:24:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import flask
+from nmapfun import *
+from flask import request
 from flask import Flask
 
 app = Flask(__name__,
@@ -18,9 +20,24 @@ def menu():
     return flask.render_template('menu.html')
 
 
-@app.route('/menu')
-def nmap():
-    return flask.render_template('nmap.html')
+@app.route('/menu/nmapfun', methods=['GET', 'POST'])
+def nmapfun():
+    if request.method == 'POST':
+        action = request.form.get('action')
+        if action == 'query_myself':
+            user_ip = request.remote_addr
+            result = query(user_ip)
+            return flask.render_template('nmapfun.html', result=result)
+        elif action == 'query_other_user':
+            ip = request.form.get('ip')
+            result = query(ip)
+            return flask.render_template('nmapfun.html', result=result)
+        elif action == 'query_network':
+            ip = request.form.get('network')
+            result = query(ip)
+            return flask.render_template('nmapfun.html', result=result)
+    # 默认情况下渲染菜单页面模板
+    return flask.render_template('nmapfun.html')
 
 
 if __name__ == '__main__':
